35b6799da9ff9c605bbe1a37642cf2061425f8c4
thssos <145839483+thssos@users.noreply.github.com>
2023-10-12 12:12:32 +1100

    fixed up pausing added brightness control

--- main.ts
+++ main.ts
@@ -1,17 +1,35 @@
 let pause2 = 0
-let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P0, 13, NeoPixelMode.RGB)
 let pp = 1
 strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
 strip.setBrightness(255)
+let brightness = 255
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) && pause2 == 0) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            # # . # #
+            # # . # #
+            # # . # #
+            `)
         pause2 = 1
     } else if (input.buttonIsPressed(Button.A) && pause2 == 1) {
+        basic.showLeds(`
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # . .
+            . # . . .
+            `)
         pause2 = 0
     } else if (1 == pause2) {
         basic.pause(1)
+    } else if (input.buttonIsPressed(Button.B)) {
+        strip.setBrightness(brightness)
+        brightness += -5
     } else {
-        strip.showRainbow(pp, pp + 10)
+        strip.showRainbow(pp, pp + 5)
         pp += 1
     }
 })
